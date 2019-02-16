9ca32ab5de73d1a02ccb82e42a96c0ef2e58d4c8
Seth Gower <sdg7234@rit.edu>
2019-02-16 16:44:56 -0500

    adds seed source file for top level ent

---
+++ src/haC64.vhd
@@ -0,0 +1,26 @@
+-------------------------------------------------
+--  File:          haC64.vhd
+--
+--  Entity:        haC64
+--  Architecture:  Structural
+--  Author:        Seth Gower and Paul Kelly
+--  Created:       02/16/19
+--  Modified:      
+--  VHDL'93
+--  Description:   The following is the entity and
+--                 architectural description of a
+--                 Commodore 64 for BrickHack V at
+--                 RIT
+-------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+entity haC64 is
+    port(
+        clk     : in    std_logic
+    );
+end entity haC64;
+architecture Structural of haC64 is
+begin
+end Structural;
