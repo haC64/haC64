875efee0438f748afc3435e444b39ece9051f8ba
Seth Gower <sdg7234@rit.edu>
2019-02-17 01:34:25 -0500

    adder ready for testing

--- src/alu_comps/adder.vhd
+++ src/alu_comps/adder.vhd
@@ -19,13 +19,32 @@ use IEEE.NUMERIC_STD.ALL;
 entity adder is
     generic( bit_width : integer := 8 );
     port(
-        input   : in    std_logic_vector(bit_width-1 downto 0);
+        a       : in    std_logic_vector(bit_width-1 downto 0);
+        b       : in    std_logic_vector(bit_width-1 downto 0);
         carry   : in    std_logic;
+
         output  : out   std_logic_vector(bit_width-1 downto 0);
-        psr     : out   std_logic_vector(3 downto 0)
+        n,v,z,c : out   std_logic
     );
 end entity adder;
 architecture Behavioral of adder is
-
+    signal s_carry     : std_logic_vector(bit_width downto 0);
+    signal s_output    : std_logic_vector(bit_width-1 downto 0);
 begin
+    output      <= s_output;
+    s_carry(0)  <= carry;
+    n           <= s_output(bit_width-1);
+    c           <= s_carry(bit_width);
+    v           <= s_carry(bit_width) xor s_carry(bit_width-1);
+
+    adders:for i in 0 to bit_width-1 generate
+        s_output(i) <= a(i) xor b(i);
+        s_carry(i+1)<= (a(i) and b(i)) or (a(i) and s_carry(i)) or (b(i) and
+                       s_carry(i));
+    end generate;
+
+    psr_proc:process(s_output)
+    begin
+
+    end process;
 end Behavioral;
