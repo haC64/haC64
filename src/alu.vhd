b6c289d47610255913cc91956378a60a7d177ca0
Seth Gower <sdg7234@rit.edu>
2019-02-17 19:31:27 -0500

    makes APSR output for ALU synchronous

--- src/alu.vhd
+++ src/alu.vhd
@@ -5,12 +5,12 @@
 --  Architecture:  Structural
 --  Author:        Paul Kelly
 --  Created:       02/16/19
---  Modified:      
+--  Modified:
 --  VHDL'93
 --  Description:   10 function arithmetic
---                 logic unit utilizing 
---                 various 8-bit binary 
---                 components 
+--                 logic unit utilizing
+--                 various 8-bit binary
+--                 components
 -------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
@@ -19,8 +19,8 @@ use IEEE.NUMERIC_STD.ALL;
 entity alu is
     PORT (
         op1, op2    : in std_logic_vector(7 downto 0); -- arithmetic operands
-        clk         : in std_logic; 
-        c_in        : in std_logic; -- carry in 
+        clk         : in std_logic;
+        c_in        : in std_logic; -- carry in
         op_code     : in std_logic_vector(3 downto 0); -- code to determine op
         rts         : out std_logic_vector(7 downto 0);
         n_out       : out std_logic; -- negative flag out
@@ -75,7 +75,7 @@ architecture Structural of alu is
             i       : in std_logic_vector(7 downto 0);
             c_in    : in std_logic;
             o       : out std_logic_vector(7 downto 0);
-            c_out   : out std_logic; 
+            c_out   : out std_logic;
             n, z    : out std_logic
         );
     end Component;
@@ -86,7 +86,7 @@ architecture Structural of alu is
             i       : in std_logic_vector(7 downto 0);
             c_in    : in std_logic;
             o       : out std_logic_vector(7 downto 0);
-            c_out   : out std_logic; 
+            c_out   : out std_logic;
             n, z    : out std_logic
         );
     end Component;
@@ -106,39 +106,49 @@ architecture Structural of alu is
     signal rol_res  : std_logic_vector(7 downto 0);
     signal ror_res  : std_logic_vector(7 downto 0);
 
+    signal nvzc_vec: std_logic_vector(3 downto 0);
 begin
     -- exclusive or
-    eor_comp : eor 
-        port map (in1 => op1, in2 => op2, o => eor_res, n => n_out, z => z_out );
+    eor_comp : eor
+        port map (in1 => op1, in2 => op2, o => eor_res, n => nvzc_vec(3), z =>
+                 nvzc_vec(1));
 
     -- increment
-    inc_comp : inc 
-        port map (i => op1, o => inc_res, n => n_out, z => z_out );
+    inc_comp : inc
+        port map (i => op1, o => inc_res, n => nvzc_vec(3), z =>
+                 nvzc_vec(1));
 
     -- logical shift right
     lsr_comp : lsr
-        port map (i => op1, o => lsr_res, n => n_out, z => z_out, c => c_out );
+        port map (i => op1, o => lsr_res, n => nvzc_vec(3), z =>
+                 nvzc_vec(1), c => nvzc_vec(0));
 
     -- or component
     ora_comp : ora
-        port map (in1 => op1, in2 => op2, o => ora_res, n => n_out, z => z_out );
+        port map (in1 => op1, in2 => op2, o => ora_res, n => nvzc_vec(3), z =>
+                 nvzc_vec(1));
 
     -- rotate left
-    rotl_comp : rotl 
-        port map (i => op1, c_in => c_in, o => rol_res, 
-                  c_out => c_out, n => n_out, z => z_out );
+    rotr_comp : rotl
+        port map (i => op1, c_in => c_in, o => rol_res,
+                  c_out => nvzc_vec(0), n => nvzc_vec(3), z => nvzc_vec(1));
 
     -- rotate right
-    rotl_comp : rotr 
-        port map (i => op1, c_in => c_in, o => ror_res, 
-                  c_out => c_out, n => n_out, z => z_out );
+    rotl_comp : rotr
+        port map (i => op1, c_in => c_in, o => ror_res,
+                  c_out => nvzc_vec(0), n => nvzc_vec(3), z => nvzc_vec(1));
 
     -- implement other components here
 
     -- issues: flags are set asynchronously
-    process(clk) is 
+    process(clk) is
     begin
         if rising_edge(clk) then
+            n_out <= nvzc_vec(3);
+            v_out <= nvzc_vec(2);
+            z_out <= nvzc_vec(1);
+            c_out <= nvzc_vec(0);
+
             case op_code is
                 when "0000" => rts <= adc_res;
                 when "0001" => rts <= and_res;
@@ -150,7 +160,7 @@ begin
                 when "0111" => rts <= lsr_res;
                 when "1000" => rts <= ora_res;
                 when "1001" => rts <= rol_res;
-                when "1010" => rts <= ror_res;
+                when others => rts <= ror_res;
             end case;
         end if;
     end process;
